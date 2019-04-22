95fcf41598a1e9b988d7bb5a5af117af36332dde
andyhkwok <andy.kwok@ifsalpha.com>
2019-04-22 11:55:51 +0200

    changement func_tests.py et .gitignore

--- func_tests.py
+++ func_tests.py
@@ -1,15 +1,39 @@
+"""
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+#browser = webdriver.Firefox()
 
 browser.get('http://127.0.0.1:8000')
 
 assert 'Django Jamie Foxx' in browser.title
 
-#from selenium import webdriver
-#
-#browser = webdriver.Firefox()
-#
-#assert 'To-Do' in browser.title
-#
-#browser.quit()
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+assert 'To-Do' in browser.title
+
+browser.quit()
+"""
+
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+
+	def tearDown(self):
+			self.browser.quit()
+
+
+	def test_can_start_a_list_and_retrieve_it(self):
+		self.browser.get('http://localhost:8000')	
+
+		self.assertIn('To-Do- Jamie Foxx',self.browser.title)
+		self.fail('Finish the test:')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')			
