18c193f49fbaefdccb41f14bceab8c422977504d
andyhkwok <andy.kwok@ifsalpha.com>
2019-04-22 14:48:02 +0200

    1er test unitaire et mapping url, avec vue dummy

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    url(r'^$', views.home_page,name='home'),
 ]
