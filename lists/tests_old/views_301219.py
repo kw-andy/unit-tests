741eddf1a0e9f07ff886a1175c4415c7d6135c6c
kw-andy <andy.kwok.work@gmail.com>
2019-12-30 17:00:38 +0100

    30/12/19 - p112/p72

--- lists/tests_old/views_301219.py
+++ lists/tests_old/views_301219.py
@@ -8,10 +8,6 @@ from django.shortcuts import render
 # home_page = None p100/60
 
 def home_page(request):
-	item = item()
-	item.text = request.POST.get('item_text','')
-	item.save()
-
 	return render(request,'home.html',{
-		'new_item_text': item.text
+		'new_item_text': request.POST.get('item_text',''),
 	})	
